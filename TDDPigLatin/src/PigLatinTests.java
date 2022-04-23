377c10be221dde848906c1e296ddaace4fd98e54
clara <clararbalmer@gmail.com>
2022-04-23 11:44:06 -0400

    translator now handles different types of punctuation

--- TDDPigLatin/src/PigLatinTests.java
+++ TDDPigLatin/src/PigLatinTests.java
@@ -5,16 +5,146 @@ import org.junit.jupiter.api.Test;
 
 class PigLatinTests {
 	
-	public static void hi() {
-		System.out.println();
-	}
+
+	
 	
 	@Test
-	void test() {
-		String expected = "";
-		String actual = PigLatin.translate("");
+	void testTranslateExtended1() {
+		String expected = "an'tcay";
+		String actual = PigLatin.translate("can't");
 		Assert.assertEquals(expected, actual);
 	}
+	@Test
+	void testTranslateExtended2() {
+		String expected = "1984";
+		String actual = PigLatin.translate("1984");
+		Assert.assertEquals(expected, actual);
+	}
+	@Test
+	void testTranslateExtended3() {
+		String expected = "hello@grandcircus.co";
+		String actual = PigLatin.translate("hello@grandcircus.co");
+		Assert.assertEquals(expected, actual);
+	}
+	@Test
+	void testTranslateExtended4() {
+		String expected = "elloHay erethay"; //changed for extended challenge
+		String actual = PigLatin.translate("Hello there");
+		Assert.assertEquals(expected, actual);
+	}
+	@Test
+	void testTranslateExtended5() {
+		String expected = "Itway ookslay ikelay ainray"; //changed for extended challenge
+		String actual = PigLatin.translate("It looks like rain");
+		Assert.assertEquals(expected, actual);
+	}
+	@Test
+	void testTranslateExtended6() {
+		String expected = "my emailway isway hello@grandcircus.co";
+		String actual = PigLatin.translate("my email is hello@grandcircus.co");
+		Assert.assertEquals(expected, actual);
+	}
+	@Test
+	void testTranslateExtended7() {
+		String expected = "unctuationpay, aymay ebay away oblempray";
+		String actual = PigLatin.translate("punctuation, may be a problem");
+		Assert.assertEquals(expected, actual);
+	}
+	@Test
+	void testTranslateExtended8() {
+		String expected = "Iway inkthay, Iway avehay- ellway, ixedfay! ethay? unctuationpay oblempray."; //changed for extended challenge
+		String actual = PigLatin.translate("I think, I have- well, fixed! the? punctuation problem.");
+		Assert.assertEquals(expected, actual);
+	}
+	@Test
+	void testTranslateExtended9() {
+		String expected = "\"elloHay erethay,\" aidsay ethay anmay."; //changed for extended challenge
+		String actual = PigLatin.translate("\"Hello there,\" said the man.");
+		Assert.assertEquals(expected, actual);
+	}
+	@Test
+	void testTranslateExtended10() {
+		String expected = "illWay itway orkway... ikelay isthay?"; //changed for extended challenge
+		String actual = PigLatin.translate("Will it work... like this?");
+		Assert.assertEquals(expected, actual);
+	}
+	@Test
+	void testTranslateExtended11() {
+		String expected = "isthay/atthay";
+		String actual = PigLatin.translate("this/that");
+		Assert.assertEquals(expected, actual);
+	}
+	@Test
+	void testTranslateExtended12() {
+		String expected = "asterMay omleyBray-atcherThay";
+		String actual = PigLatin.translate("Master Bromley-Thatcher");
+		Assert.assertEquals(expected, actual);
+	}
+	@Test
+	void testTranslateExtended13() {
+		String expected = "applesway/ananasbay/errieschay/umquatskay/avocadosway/ustn'tmay";
+		String actual = PigLatin.translate("apples/bananas/cherries/kumquats/avocados/mustn't");
+		Assert.assertEquals(expected, actual);
+	}
+	@Test
+	void testTranslateExtended14() {
+		String expected = "uperSay-alicay-agifray-isticlay-expiway-aliway-ociousday";
+		String actual = PigLatin.translate("Super-cali-fragi-listic-expi-ali-docious");
+		Assert.assertEquals(expected, actual);
+	}
+	@Test
+	void testTranslateExtended15() {
+		String expected = "(et'sLay eesay ifway isthay orksway.)";
+		String actual = PigLatin.translate("(Let's see if this works.)");
+		Assert.assertEquals(expected, actual);
+	}
+	@Test
+	void testTranslateExtended16() {
+		String expected = "isthay&atthay";
+		String actual = PigLatin.translate("this&that");
+		Assert.assertEquals(expected, actual);
+	}
+	
+	
+	@Test
+	void testContainsEndPunct1() {
+		boolean expected = true;
+		boolean actual = PigLatin.endsWithPunctuation("hello,");
+		Assert.assertEquals(expected, actual);
+	}
+	@Test
+	void testContainsEndPunct2() {
+		boolean expected = true;
+		boolean actual = PigLatin.endsWithPunctuation("Oh no.");
+		Assert.assertEquals(expected, actual);
+	}
+	@Test
+	void testContainsEndPunct3() {
+		boolean expected = false;
+		boolean actual = PigLatin.endsWithPunctuation("munchkin");
+		Assert.assertEquals(expected, actual);
+	}
+	
+	
+	@Test
+	void testContainsSpecial1() {
+		boolean expected = false;
+		boolean actual = PigLatin.containsSpecial("hello");
+		Assert.assertEquals(expected, actual);
+	}
+	@Test
+	void testContainsSpecial2() {
+		boolean expected = true;
+		boolean actual = PigLatin.containsSpecial("fjdkslfj@fdjskl");
+		Assert.assertEquals(expected, actual);
+	}
+	@Test
+	void testContainsSpecial3() {
+		boolean expected = false;
+		boolean actual = PigLatin.containsSpecial("");
+		Assert.assertEquals(expected, actual);
+	}
+	
 	
 	@Test
 	void testTranslate1() {
@@ -24,13 +154,13 @@ class PigLatinTests {
 	}
 	@Test
 	void testTranslate2() {
-		String expected = "away";
+		String expected = "Away"; //changed for extended challenge
 		String actual = PigLatin.translate("A");
 		Assert.assertEquals(expected, actual);
 	}
 	@Test
 	void testTranslate3() {
-		String expected = "aberforthway";
+		String expected = "Aberforthway"; //changed for extended challenge
 		String actual = PigLatin.translate("Aberforth");
 		Assert.assertEquals(expected, actual);
 	}
@@ -43,7 +173,7 @@ class PigLatinTests {
 	@Test
 	void testTranslate5() {
 		String expected = "iglooway";
-		String actual = PigLatin.translate("Igloo");
+		String actual = PigLatin.translate("igloo");
 		Assert.assertEquals(expected, actual);
 	}
 	@Test
@@ -84,7 +214,7 @@ class PigLatinTests {
 	}
 	@Test
 	void testTranslate12() {
-		String expected = "uyenngay";
+		String expected = "uyenNgay"; //changed to keeping case for extended challenge
 		String actual = PigLatin.translate("Nguyen");
 		Assert.assertEquals(expected, actual);
 	}
@@ -123,8 +253,8 @@ class PigLatinTests {
 	
 	@Test
 	void testTranslateConsonant1() {
-		String expected = "arkpay";
-		String actual = PigLatin.translateConsonantWord("park");
+		String expected = "iraffegay";
+		String actual = PigLatin.translateConsonantWord("giraffe");
 		Assert.assertEquals(expected, actual);
 	}
 	@Test
@@ -155,8 +285,8 @@ class PigLatinTests {
 	}
 	@Test
 	void testTranslateVowel2() {
-		String expected = "errandway";
-		String actual = PigLatin.translateVowelWord("errand");
+		String expected = "elseway";
+		String actual = PigLatin.translateVowelWord("else");
 		Assert.assertEquals(expected, actual);
 	}
 	@Test
